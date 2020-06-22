692c04474ef18f3ce0a4896e196e13ac26f60b90
Richard <richardjimenez176@gmail.com>
2020-06-21 21:42:15 -0400

    add markdown instead of just doing my own text styling thing

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import redis
 
 app=Flask(__name__)
-
+r=redis.Redis()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/read")
+def read():
+    return render_template("read.html")
+
+@app.route("/read_file")
+def read_file():
+    return str(r.get("text").decode())
 
 if __name__ == "__main__":
     app.run(debug=True)
